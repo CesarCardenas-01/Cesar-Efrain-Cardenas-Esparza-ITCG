61ea76045b1db8e7fa8a685054e86a9107948f5a
CesarCardenas-01 <78033394+CesarCardenas-01@users.noreply.github.com>
2023-09-24 11:37:00 -0600

    Add files via upload

---
+++ Agente Vendedor - Comprador/behaviours/OfferRequestServer.java
@@ -0,0 +1,42 @@
+package behaviours;
+
+import agentes.BookSellerAgent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+/**
+ *
+ * @author Cesar
+ */
+public class OfferRequestServer extends CyclicBehaviour{
+
+    BookSellerAgent bsAgent;
+  
+  public OfferRequestServer(BookSellerAgent a) {
+    bsAgent = a;
+  }
+  
+  public void action() {
+    MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+    ACLMessage msg = bsAgent.receive(mt);
+    
+    if(msg != null) {
+      String title = msg.getContent();
+      ACLMessage reply = msg.createReply();
+      
+      Integer price = (Integer) bsAgent.getCatalogue().get(title);
+      
+      if(price != null) {
+        reply.setPerformative(ACLMessage.PROPOSE);
+        reply.setContent(String.valueOf(price.intValue()));
+      } else {
+        reply.setPerformative(ACLMessage.REFUSE);
+        reply.setContent("not-available");
+      }
+      
+      bsAgent.send(reply);
+    } else {
+      block();
+    }
+  }
+}
